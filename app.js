c37ff1a7548f4fa230037692c2f0077904858173
illinia <67290425+illinia@users.noreply.github.com>
2021-05-20 22:23:32 +0900

    canvas study

---
+++ app.js
@@ -0,0 +1,43 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d")
+
+canvas.width = 700
+canvas.height = 700
+
+ctx.strokeStyle = "#2c2c2c"
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function stopPainting() {
+  painting = false;
+}
+
+function startPainting() {
+  painting = true;
+}
+
+function onMouseMove(e) {
+  const x = e.offsetX;
+  const y = e.offsetY;
+  if (!painting) {
+    console.log("creating path in ", x, y)
+    ctx.beginPath()
+    ctx.moveTo(x, y)
+  } else {
+    console.log("creating line in ", x, y)
+    ctx.lineTo(x, y)
+    ctx.stroke()
+  }
+}
+
+function onMouseDown(e) {
+  painting = true;
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove)
+  canvas.addEventListener("mousedown", startPainting)
+  canvas.addEventListener("mouseup", stopPainting)
+  canvas.addEventListener("mouseleave", stopPainting)
+}
